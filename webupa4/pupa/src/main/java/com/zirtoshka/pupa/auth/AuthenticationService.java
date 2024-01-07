e35d2bc1043d9fbd228fc7091bb6097fd3955399
zirtoshka <367222@edu.itmo.ru>
2024-01-07 21:11:14 +0300

    auth + register

---
+++ webupa4/pupa/src/main/java/com/zirtoshka/pupa/auth/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.zirtoshka.pupa.auth;
+
+import com.zirtoshka.pupa.config.JwtService;
+import com.zirtoshka.pupa.user.Role;
+import com.zirtoshka.pupa.user.User;
+import com.zirtoshka.pupa.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user= User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
