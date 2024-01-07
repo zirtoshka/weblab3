91afd41ae4dedee58fa5e2538525f816248a07aa
zirtoshka <367222@edu.itmo.ru>
2024-01-07 21:17:23 +0300

    auth + authenticate

--- webupa4/pupa/src/main/java/com/zirtoshka/pupa/auth/AuthenticationService.java
+++ webupa4/pupa/src/main/java/com/zirtoshka/pupa/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.zirtoshka.pupa.user.Role;
 import com.zirtoshka.pupa.user.User;
 import com.zirtoshka.pupa.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,10 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        var user= User.builder()
+        var user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .email(request.getEmail())
@@ -31,6 +35,17 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
