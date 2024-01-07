85ef55f2cc42faf282757cf1764599c83f663349
zirtoshka <367222@edu.itmo.ru>
2024-01-07 17:39:48 +0300

    chtoooo

--- webupa4/pupa/src/main/java/com/zirtoshka/pupa/config/JwtAuthenticationFilter.java
+++ webupa4/pupa/src/main/java/com/zirtoshka/pupa/config/JwtAuthenticationFilter.java
@@ -19,7 +19,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+        final  String authHeader =request.getHeader("Authorization");
+        final  String jwt;
+        final String userEmail;
+        if (authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt=authHeader.substring(7);
+        userEmail=//todo extract the userEmail from JWT token;
 
     }
 }
