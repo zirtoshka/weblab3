39f90f8fb32fd5a81c7798e5397a6daddf4945ce
zirtoshka <367222@edu.itmo.ru>
2024-01-20 21:30:43 +0300

    small steps to form

--- angulartrying/angular-try/src/app/compo/shot-form/shot-form.component.ts
+++ angulartrying/angular-try/src/app/compo/shot-form/shot-form.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
 import {SliderModule} from "primeng/slider";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, NgForm} from "@angular/forms";
 import {TreeSelectModule} from "primeng/treeselect";
+import {PaginatorModule} from "primeng/paginator";
+import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {InputGroupModule} from "primeng/inputgroup";
+import {ButtonModule} from "primeng/button";
 
 @Component({
     selector: 'app-shot-form',
@@ -9,11 +13,32 @@ import {TreeSelectModule} from "primeng/treeselect";
     imports: [
         SliderModule,
         FormsModule,
-        TreeSelectModule
+        TreeSelectModule,
+        PaginatorModule,
+        InputGroupAddonModule,
+        InputGroupModule,
+        ButtonModule
     ],
     templateUrl: './shot-form.component.html',
     styleUrl: './shot-form.component.css'
 })
 export class ShotFormComponent {
-    value!: number;
+    valueX=0;
+    valueR=0;
+    valueY = 0;
+
+    onSubmit(form: NgForm) {
+        const {x, y, r} = form.value;
+        console.log(x, y,r );
+        // xList.forEach((x: any) => rList.forEach((r: any) => this.shoot(x, y, r)))
+    }
+
+    isFormValid() {
+        // const {x, y, r} = form.value;
+        // console.log(x)
+        return true;
+       return (this.valueX>=-5 && this.valueX<=3) &&(this.valueY>=-5 && this.valueY<=3) &&(this.valueR>=-5 && this.valueR<=3) ;
+    }
+
+
 }
